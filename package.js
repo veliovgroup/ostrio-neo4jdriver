782d7d6d9e01189b70638810b05bbcf88aa4f400
dr.dimitru <dr.dimitru@gmail.com>
2014-11-30 19:54:06 +0300

    Add underscore dependency

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: 'ostrio:neo4jdriver',
   summary: 'Meteor.js node-neo4j wrapper to be used with meteor applications (a.k.a. neo4j Connector)',
-  version: '0.1.12',
+  version: '0.1.13',
   git: 'https://github.com/VeliovGroup/ostrio-neo4jdriver.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles('ostrio:neo4jdriver.js', 'server');
+  api.use('underscore', 'server');
 });
 
 Npm.depends({
