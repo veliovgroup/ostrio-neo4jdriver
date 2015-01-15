d3989537e6585c712e101f4413f09bf6f353d67e
dr.dimitru <dr.dimitru@gmail.com>
2015-01-15 22:55:52 +0300

    Upd due to Meteor updates
    Add on use npm-container

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ostrio:neo4jdriver',
   summary: 'Meteor.js node-neo4j wrapper to be used with meteor applications (a.k.a. neo4j Connector)',
-  version: '0.2.2',
+  version: '0.2.3',
   git: 'https://github.com/VeliovGroup/ostrio-neo4jdriver.git'
 });
 
@@ -9,6 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles('ostrio:neo4jdriver.js', 'server');
   api.use('underscore', 'server');
+  api.use('npm-container@1.0.0');
 });
 
 Npm.depends({
