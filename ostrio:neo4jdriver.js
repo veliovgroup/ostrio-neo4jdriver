3019621120879cca696e1e4abcdead93be5aae19
dr.dimitru <dr.dimitru@gmail.com>
2015-01-15 23:16:11 +0300

    whops

--- ostrio:neo4jdriver.js
+++ ostrio:neo4jdriver.js
@@ -21,7 +21,7 @@ this.Neo4j = (function() {
    *
    */
   function Neo4j(url) {
-    this.url = (url !== null) ? url : process.env['NEO4J_URL'] || process.env['GRAPHENEDB_URL'] || 'http://localhost:7474';
+    this.url = (url != null) ? url : process.env['NEO4J_URL'] || process.env['GRAPHENEDB_URL'] || 'http://localhost:7474';
     this.N4j = Meteor.npmRequire('neo4j');
     _n4j = this.N4j;
 
