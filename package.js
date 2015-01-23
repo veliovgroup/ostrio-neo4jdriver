3ab4847f0d2cbc3e1d93b57c110e114df6a57238
dr.dimitru <dr.dimitru@gmail.com>
2015-01-23 19:01:20 +0300

    Verbose
    Add console verbose logging, if :
     - DB is successfully connected
     - If connection was unsuccessful

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'ostrio:neo4jdriver',
   summary: 'Meteor.js node-neo4j wrapper to be used with meteor applications (a.k.a. neo4j Connector)',
-  version: '0.2.5',
+  version: '0.2.6',
   git: 'https://github.com/VeliovGroup/ostrio-neo4jdriver.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles('ostrio:neo4jdriver.js', 'server');
-  api.use('underscore', 'server');
+  api.use(['underscore', 'http'], 'server');
   api.use('meteorhacks:npm@1.2.2');
 });
 
