df431d1b1b76f88f4cc01fe352ab296e5298f427
dr.dimitru <ceo@veliov.com>
2015-03-01 23:54:58 +0300

    Switch from Meteor.npmRequire to Npm.require

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ostrio:neo4jdriver',
   summary: 'Meteor.js node-neo4j wrapper to be used with meteor applications (a.k.a. neo4j Connector)',
-  version: '0.2.7',
+  version: '0.2.10',
   git: 'https://github.com/VeliovGroup/ostrio-neo4jdriver.git'
 });
 
@@ -9,7 +9,6 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles('ostrio:neo4jdriver.js', 'server');
   api.use(['underscore', 'http'], 'server');
-  api.use('meteorhacks:npm@1.2.2');
 });
 
 Npm.depends({
