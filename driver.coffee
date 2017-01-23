6fb26d3574718d8113d2dac92e9d0ce7317b6126
dr-dimitru <ceo@veliov.com>
2017-01-23 13:10:09 +0300

    Get rid of CoffeeScript, add ES6 import support

--- driver.coffee
+++
@@ -1,1 +0,0 @@
-{Neo4jCursor, Neo4jRelationship, Neo4jNode, Neo4jData, Neo4jEndpoint, Neo4jTransaction, Neo4jDB} = Npm.require 'neo4j-fiber'
---
+++ driver.js
@@ -0,0 +1,2 @@
+const {Neo4jCursor, Neo4jRelationship, Neo4jNode, Neo4jData, Neo4jEndpoint, Neo4jTransaction, Neo4jDB} = Npm.require('neo4j-fiber');
+export {Neo4jCursor, Neo4jRelationship, Neo4jNode, Neo4jData, Neo4jEndpoint, Neo4jTransaction, Neo4jDB};
